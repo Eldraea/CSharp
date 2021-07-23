a6eed0af8dc8006a182f9072df2aa2e524831abe
Kimberly <kjacques709@gmail.com>
2021-07-23 11:54:30 +0200

    Second Program: Introduction to Variables and DataTypes

---
+++ Variables_And_DataTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Variables_And_DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
