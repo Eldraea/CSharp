e73442e51cbd2a4e19c280d7f19679bd7e658e1e
Kimberly <kjacques709@gmail.com>
2021-07-23 12:04:01 +0200

    Second Program: Introduction to Variables and DataTypes

--- Variables_And_DataTypes/Program.cs
+++ Variables_And_DataTypes/Program.cs
@@ -6,7 +6,17 @@ namespace Variables_And_DataTypes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int age = 21;
+            char letter = 'A';
+            bool motivation = true;
+            double height = 1.61;
+            string name = "Kim";
+
+            Console.WriteLine("Hello I am " + name);
+            Console.WriteLine("I am currently " + age);
+            Console.WriteLine("I am " + height + " meters tall");
+            Console.WriteLine("Saying that I love C# is absolutely " + motivation);
+            Console.WriteLine("If you work really hard you are sure to get an " + letter);
         }
     }
 }
