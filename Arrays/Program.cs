ba707d4d46783825326f99148442bc6564914961
Kimberly <kjacques709@gmail.com>
2021-07-27 08:11:46 +0200

    Third Program: Introduction to Arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 92, 53, 54 };
+
+            for(int i =0; i <myArray.Length;i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+
+            
+        }
+    }
+}
