a7b89a8bff39230476163899c9d7889d3f8fc7d0
Kimberly <kjacques709@gmail.com>
2021-07-31 09:39:11 +0200

    Eigth Program: Introduction to Delegates

---
+++ Delegates_Basics/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Delegates_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> names = new List<string>() { "Beatrice", "Bill", "Budd", "Elle", "Oren" };
+            Console.WriteLine("Here is our list before the remove:");
+
+            foreach (String name in names)
+                Console.WriteLine(name);
+
+            names.RemoveAll(Filter);
+
+            Console.WriteLine("\nHere is our list after the remove");
+
+            foreach (String name in names)
+                Console.WriteLine(name);
+
+
+        }
+
+        public static bool Filter(String s)
+        {
+            return s.Contains("i");
+        }
+    }
+}
