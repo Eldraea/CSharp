cdd642a78db388f8c53483de96cb2ce8b2a57961
Kimberly <kjacques709@gmail.com>
2021-08-24 11:49:35 +0200

    Eleventh Program: Introdution to Linq Library

---
+++ LinqApplication/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            OddNumbers(numbers);
+            Console.Read();
+    
+        }
+
+        static void OddNumbers(int[] numbers)
+        {
+            Console.WriteLine("Odd numbers: ");
+            IEnumerable<int> oddNumbers = from number in numbers where number % 2 != 0 select number;
+            foreach (int number in oddNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
